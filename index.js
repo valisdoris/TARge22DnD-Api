0c494e87161f0f3a6b4e76db03dbb8bfc2f5f95f
Doris Valis <valisdoris@gmail.com>
2023-11-24 16:38:23 +0200

    As an app developer I want to get a list of all services available [Delivers #186547870]

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const app = require('express')()
-const port = 8080
+const port = 8090
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
+
+app.get('/services', (req, res) => {
+  res.send(["Pedicure with gel polish", "Manicure with gel polish", "Classic manicure"])
+})
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
