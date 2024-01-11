20cfdfb599dfbac7d1044b266f7857b49084d015
Diana Sedman <dianasedman@gmail.com>
2024-01-11 17:03:37 +0200

    frontent for index page

--- DnD/TARge22DnD-Api/src/App.vue
+++ DnD/TARge22DnD-Api/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/servicedetails">About</RouterLink>
       </nav>
     </div>
   </header>
--- DnD/TARge22DnD-Api/src/router/index.js
+++ DnD/TARge22DnD-Api/src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/servicedetails',
+      path: '../views/chooseservice/servicedetailsview',
       name: 'servicedetails',
       component: () => import("../views/chooseservice/servicedetailsview.vue")
     },
